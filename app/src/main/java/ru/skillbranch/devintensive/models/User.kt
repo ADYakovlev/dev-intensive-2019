d4c8636cf6441c77f5ae65c7df4827f062065141
yakovlev <ay@skb.ru>
2019-11-09 23:48:11 +0300

    home task 2

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,63 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+data class User(
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var reiting: Int = 0,
+    var respect: Int = 0,
+    val lastVisit: Date? = null,
+    val isOnline: Boolean = false
+) {
+    var introBit: String
+
+    constructor(id: String, firstName: String?, lastName: String?) : this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    constructor(id: String) : this(id, "John", "Doe")
+
+    init {
+        introBit = getIntro()
+        println(
+            "It's Alive!!! ${if (lastName === "Doe") "His name id $firstName $lastName" else "And his name $firstName $lastName"}\n" +
+                    "${getIntro()}"
+        )
+    }
+
+    fun getIntro() = """
+
+        tu tu tu !!!
+        $firstName $lastName
+
+    """.trimIndent()
+
+    fun printMe() = println(
+        """
+            id: $id
+            firstName: $firstName
+            lastName: $lastName
+            avatar: $avatar
+            reiting: $reiting
+            respect: $respect
+            lastVisit: $lastVisit
+            isOnline: $isOnline
+        """.trimIndent()
+    )
+
+    companion object Factory {
+        private var lastId: Int = -1
+        fun makeUser(fullName: String?): User {
+            lastId++
+            val (firstName, lastName) = Utils.parseFullName(fullName)
+            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,100 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    //TODO FIX ME!!!
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String): String {
+        val result = StringBuilder()
+        for (i in 0..payload.length - 1) {
+            result.append(Utils.translitChar(payload.get(i).toString()))
+        }
+        return result.toString()
+    }
+
+    fun translitChar(c: String): String {
+        return when (c) {
+            "А" -> "A"
+            "Б" -> "B"
+            "В" -> "V"
+            "Г" -> "G"
+            "Д" -> "D"
+            "Е" -> "E"
+            "Ё" -> "Je"
+            "Ж" -> "Zh"
+            "З" -> "Z"
+            "И" -> "I"
+            "Й" -> "Y"
+            "К" -> "K"
+            "Л" -> "L"
+            "М" -> "M"
+            "Н" -> "N"
+            "О" -> "O"
+            "П" -> "P"
+            "Р" -> "R"
+            "С" -> "S"
+            "Т" -> "T"
+            "У" -> "U"
+            "Ф" -> "F"
+            "Х" -> "Kh"
+            "Ц" -> "C"
+            "Ч" -> "Ch"
+            "Ш" -> "Sh"
+            "Щ" -> "Sh"
+            "Ъ" -> ""
+            "Ы" -> "Ih"
+            "Ь" -> ""
+            "Э" -> "Eh"
+            "Ю" -> "U"
+            "Я" -> "Ya"
+            "а" -> "a"
+            "б" -> "b"
+            "в" -> "v"
+            "г" -> "g"
+            "д" -> "d"
+            "е" -> "e"
+            "ё" -> "je"
+            "ж" -> "zh"
+            "з" -> "z"
+            "и" -> "i"
+            "й" -> "y"
+            "к" -> "k"
+            "л" -> "l"
+            "м" -> "m"
+            "н" -> "n"
+            "о" -> "o"
+            "п" -> "p"
+            "р" -> "r"
+            "с" -> "s"
+            "т" -> "n"
+            "у" -> "u"
+            "ф" -> "f"
+            "х" -> "kh"
+            "ц" -> "c"
+            "ч" -> "ch"
+            "ш" -> "sh"
+            "щ" -> "jsh"
+            "ъ" -> ""
+            "ы" -> "i"
+            "ь" -> ""
+            "э" -> "e"
+            "ю" -> "u"
+            "я" -> "ya"
+            else -> c
+        }
+    }
+
+    fun toInitials(firstName: String?, middleName: String?): String? {
+        val initials = StringBuilder();
+        initials.append(firstName?.get(0)?.toUpperCase())
+        initials.append(". ")
+        initials.append(middleName?.get(0)?.toUpperCase())
+        initials.append(".")
+        return initials.toString()
+    }
+}
